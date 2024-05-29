5a7a065f2d1b290d58fe2b1cf33d922485be2036
Mireya <mmedmer810@g.educaand.es>
2024-05-29 18:15:44 +0200

    Borrado de código innecesario

--- src/main/java/org/iesvdm/fctconnect/service/EmpresaService.java
+++ src/main/java/org/iesvdm/fctconnect/service/EmpresaService.java
@@ -1,12 +1,10 @@
 package org.iesvdm.fctconnect.service;
 
-import org.iesvdm.fctconnect.domain.Alumno;
 import org.iesvdm.fctconnect.domain.Empresa;
 import org.iesvdm.fctconnect.domain.enums.EInglesSolicitado;
 import org.iesvdm.fctconnect.domain.enums.EModalidadTrabajo;
 import org.iesvdm.fctconnect.exception.EntityNotFoundException;
 import org.iesvdm.fctconnect.repository.EmpresaRepository;
-import org.iesvdm.fctconnect.repository.CustomQueryImpl;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -22,26 +20,14 @@ import java.util.Optional;
 public class EmpresaService {
     private final EmpresaRepository empresaRepository;
 
-    private final CustomQueryImpl customQuery;
-
-    public EmpresaService(EmpresaRepository empresaRepository, CustomQueryImpl empresaRepositoryCustomQuery) {
+    public EmpresaService(EmpresaRepository empresaRepository) {
         this.empresaRepository = empresaRepository;
-        this.customQuery = empresaRepositoryCustomQuery;
     }
 
     public List<Empresa> all() {
         return this.empresaRepository.findAll();
     }
-//
-//    public Map<String, Object> buscarEmpresaPaginacion (Optional<String> nombreOpt,
-//                                                        Optional<String> modalidadTrabajoOpt,
-//                                                        Optional<String> inglesSolicitadoOpt,
-//                                                        Optional<String> orderOpt,
-//                                                        Optional<Integer> paginaOpt,
-//                                                        Optional<Integer> tamanioOpt) {
-//        return this.customQuery.buscarEmpresaPaginacion(nombreOpt, modalidadTrabajoOpt, inglesSolicitadoOpt,
-//                orderOpt, paginaOpt, tamanioOpt);
-//    }
+
 
     public Map<String, Object> all(int pagina, int tamanio) {
         Pageable paginado = PageRequest.of(pagina, tamanio, Sort.by("id").ascending());
@@ -74,61 +60,6 @@ public class EmpresaService {
 
         return response;
     }
-
-
-
-
-    // Búsquedas por nombre por ingles+modalidad de trabajo y paginación
-//
-//        public List<Empresa> buscarPorNombre (Optional<String> buscarOpt, Optional<String> ordenarOpt) {
-//        if (buscarOpt.isPresent() && ordenarOpt.isPresent()) {
-//            if (ordenarOpt.get().equals("asc")) {
-//                return this.empresaRepository.findEmpresaByNombreContainingIgnoreCaseOrderByNombreAsc(buscarOpt.get());
-//            } else if (buscarOpt.get().equals("desc")){
-//                return this.empresaRepository.findEmpresaByNombreContainingIgnoreCaseOrderByNombreDesc(buscarOpt.get());
-//            }
-//        } else if (!buscarOpt.isPresent() && ordenarOpt.isPresent()) {
-//            if (ordenarOpt.get().equals("asc")) {
-//                return this.empresaRepository.findAllByOrderByNombreAsc();
-//            } else if (buscarOpt.get().equals("desc")){
-//                return this.empresaRepository.findAllByOrderByNombreDesc();
-//            }
-//        } else if (buscarOpt.isPresent() && !ordenarOpt.isPresent()) {
-//            return this.empresaRepository.findEmpresaByNombreContainingIgnoreCase(buscarOpt.get());
-//        }
-//        return this.empresaRepository.findAll();
-//    }
-//
-//    public List<Empresa> buscarPorInglesYModalidad (Optional<String> inglesSolicitado, Optional<String> modalidadTrabajo) {
-//        if (inglesSolicitado.isPresent() && modalidadTrabajo.isPresent()) {
-//            return this.empresaRepository.findEmpresaByInglesSolicitadoContainingIgnoreCaseAndModalidadTrabajoContainingIgnoreCase(inglesSolicitado.get(), modalidadTrabajo.get());
-//        } else if (inglesSolicitado.isPresent() && !modalidadTrabajo.isPresent()) {
-//            return this.empresaRepository.findEmpresaByInglesSolicitadoContainingIgnoreCase(inglesSolicitado.get());
-//        } else if (!inglesSolicitado.isPresent() && modalidadTrabajo.isPresent()) {
-//            return this.empresaRepository.findEmpresaByModalidadTrabajoContainingIgnoreCase(modalidadTrabajo.get());
-//        }
-//
-//        return this.empresaRepository.findAll();
-//    }
-
-//
-//    public Map<String, Object> paginacion (String[] paginado) {
-//        int pagina = Integer.parseInt(paginado[0]);
-//        int tamanio =  Integer.parseInt(paginado[1]);
-//
-//        Pageable paginacion = PageRequest.of(pagina, tamanio, Sort.by("id").ascending());
-//        Page<Empresa> pageAll = this.empresaRepository.findAll(paginacion);
-//
-//        Map<String, Object> response = new HashMap<>();
-//
-//        response.put("empresas", pageAll.getContent());
-//        response.put("currentPage", pageAll.getNumber());
-//        response.put("totalItems", pageAll.getTotalElements());
-//        response.put("totalPages", pageAll.getTotalPages());
-//
-//        return response;
-//    }
-
 
 
     public Empresa save(Empresa empresa) {
